bf6dbe629108740b02b7bb326c1e698ef11057f0
carog <caroline.gomot@gmail.com>
2022-04-16 18:55:37 +0200

    wrote js file for index (a, article, img, h3, p). Linked to api with object productenter the commit message for your changes. Lines starting

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,54 @@
 
+var maSection = document.getElementById("items");
+
+/*var articlesCanap = (product);
+console.log(articlesCanap[0]);
+console.log(articlesCanap[2].name);
+
+var mesArticlesLiens = document.createElement("a");
+mesArticlesLiens.href = "#";
+console.log(mesArticlesLiens);
+var mesArticlesArticles = document.createElement("article");
+var mesArticlesImg = document.createElement("img");
+var mesArticlesNomProduit = document.createElement("h3");
+var mesArticlesDescription = document.createElement("p");
+// mesArticlesImg.alt = //articlesCanap[i].altTxt;*/
+
+
+
+function nouveauLien(product){
+   lienSection =  document.createElement("a");
+   items.appendChild(lienSection);
+   lienSection.setAttribute("href","./product.html?id="+product._id);
+}
+
+function nouvelArticle(){
+    articleSection = document.createElement("article")
+    lienSection.appendChild(articleSection)
+    }
+
+function nouvelleImg(product){
+    imgSection = document.createElement("img");
+    articleSection.appendChild(imgSection);
+    imgSection.setAttribute("src",product.imageUrl);
+    imgSection.setAttribute("alt",product.altTxt);
+
+}
+
+function nouveauTitreProduit(product){
+    titreSection = document.createElement("h3");
+    articleSection.appendChild(titreSection);
+    titreSection.classList.add('productName');
+    titreSection.textContent = product.name;
+}
+
+function nouvelleDescription(product){
+    descriptionSection = document.createElement("p");
+    articleSection.appendChild(descriptionSection);
+    descriptionSection.classList.add('productDescription');
+    descriptionSection.textContent = product.description;
+}
+
 // appeler l'API
 
 fetch('http://localhost:3000/api/products')
@@ -7,9 +57,19 @@ fetch('http://localhost:3000/api/products')
         return res.json();
     }
 })
-.then(function(value){
-    console.log(value);
-})
+.then(function(data) {
+    for (let i in data) {
+        nouveauLien(data[i])
+        nouvelArticle(data[i])
+        nouvelleImg(data[i])
+        nouveauTitreProduit(data[i])
+        nouvelleDescription(data[i])
+    }})
+
+
+
+
 .catch(function(err){
     //Une erreur est survenue
 });
+
