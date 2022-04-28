d75452353cb6c6ed82d435b9e1683ec999e36240
carog <caroline.gomot@gmail.com>
2022-04-28 16:41:13 +0200

    test panier changed basket.js to original

--- front/js/basket.js
+++ front/js/basket.js
@@ -1,49 +1,4 @@
-//----- 1. Afficher les produits du panier et leurs caractéristiques, faire les totaux de prix et quantité, gérer les modifications et suppression -----
 
-//Accès aux élements du DOM
-const cartItems = document.getElementById("cart__items")
-
-//Récupération des informations du panier contenues dans localStorage
-let cartArray
-if (localStorage.length !=0) { cartArray = JSON.parse(localStorage.cart)}
-
-//Déclaration des variables globales
-let newArticle
-let nodeParent
-let totalPrice = 0
-let totalQuantity = 0
-
-//Affichage d'un message d'alerte au chargement de la page si le panier est vide ou inexistant
-document.onreadystatechange = function () {
-    if (document.readyState == 'complete' && (cartArray == undefined || cartArray.length == 0)) {
-        alert("Votre panier est vide!\nVeuillez sélectionner vos articles et les ajouter au panier avant de remplir le formulaire pour transmettre votre commande.")
-    }
-}
-
-//Affichage du panier
-for (let i in cartArray) {
-    //Requête sur l'API pour récupérer les informations de chaque produit du panier
-    fetch("http://localhost:3000/api/products/" + cartArray[i]._id)
-        //Si la requête aboutie, parser les données .json contenues dans le body de la réponse en objet JS
-        .then(function (response) {
-            if (response.ok) {
-                return response.json()
-            }
-        })
-        .then(function (data) {
-            cartArray[i].name = data.name
-            cartArray[i].price = data.price
-            cartArray[i].imageUrl = data.imageUrl
-            cartArray[i].altTxt = data.altTxt
-            viewCart(i)
-            //Calcul des totaux prix et quantité
-            updateTotalPriceAndQuantity()
-        })
-        //En cas d'erreur, affichage du message correspondant dans la console
-        .catch(function (err) {
-            console.log(err.message)
-        })
-}
 
 
 
